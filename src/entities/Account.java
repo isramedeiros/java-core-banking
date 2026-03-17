f14cc9897a6ad274f90127b518a5d52f19093ae6
Israel Medeiros <israelmfreitas12@gmail.com>
2026-03-17 09:30:17 -0300

    Initial commit

---
+++ src/entities/Account.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class Account {
+    private int number;
+    private String holder;
+    private double balance;
+
+    // method overloading below
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    public Account(int number, String holder, double initialDeposit) {
+        this.number = number;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    // deposit method
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    // withdraw method
+    public void withdraw(double amount) {
+        balance -= amount + 5.0;
+    }
+
+    public String toString() {
+        return "Account "
+                + number
+                + ", Holder: "
+                + holder
+                + ", Balance: $"
+                + String.format("%.2f", balance);
+    }
+}
